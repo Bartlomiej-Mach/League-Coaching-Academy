626f574528131ec8e556392db2859fded921e7d8
unknown <bartekmachu@gmail.com>
2021-10-03 13:04:01 +0200

    mobile

--- js/nav/nav-animation.js
+++ js/nav/nav-animation.js
@@ -8,3 +8,15 @@ tl.from('.navigation-section__logo-container', {
 tl.from('.navigation-section__nav-list-container', {
     opacity: 0,
 }, "<");
+
+
+
+const navList = document.querySelector('.navigation-section__nav-list-container');
+const navBtn = document.querySelector('.navigation-section__button-container');
+
+const openCloseNav = () => {
+    navList.classList.toggle('nav-list--active');
+    navBtn.classList.toggle('nav-btn--active');
+}
+
+navBtn.addEventListener('click', openCloseNav);
